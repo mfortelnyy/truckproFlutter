f4d5d16bab01889c326e9a20d24a08a04cb75b3c
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-24 23:31:15 -0500

    Add log entry type enum

--- truckpro/lib/models/log_entry_type.dart.cs
+++ truckpro/lib/models/log_entry_type.dart.cs
@@ -2,6 +2,19 @@ namespace lib.models
 {
     public enum log_entry_type.dart
     {
+        enum LogEntryType {
+            Driving, 
+            OnDuty,
+            Break,
+            OffDuty
+        }
+
+        extension LogEntryTypeExtension on LogEntryType {
+            String toShortString() {
+                return toString().split('.').last;
+            }
+        }
+
         
     }
 }
