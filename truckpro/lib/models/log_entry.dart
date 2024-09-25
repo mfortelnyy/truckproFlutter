4399f4e20d5811a5ddb531893322119c3ceb3922
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-24 23:13:13 -0500

    Add logentry model

---
+++ truckpro/lib/models/log_entry.dart
@@ -0,0 +1,39 @@
+class LogEntry {
+  final String id;
+  final String userId;
+  final DateTime startTime;
+  final DateTime endTime;
+  final String logEntryType;
+  final List<String>? images;
+
+  LogEntry({
+    required this.id,
+    required this.userId,
+    required this.startTime,
+    required this.endTime,
+    required this.logEntryType,
+    this.images,
+  });
+
+  factory LogEntry.fromJson(Map<String, dynamic> json) {
+    return LogEntry(
+      id: json['id'],
+      userId: json['userId'],
+      startTime: DateTime.parse(json['startTime']),
+      endTime: DateTime.parse(json['endTime']),
+      logEntryType: json['logEntryType'],
+      images: json['images'] != null ? List<String>.from(json['images']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'userId': userId,
+      'startTime': startTime.toIso8601String(),
+      'endTime': endTime.toIso8601String(),
+      'logEntryType': logEntryType,
+      'images': images,
+    };
+  }
+}
---
+++ truckpro/lib/models/log_entry_type.dart.cs
@@ -0,0 +1,7 @@
+namespace lib.models
+{
+    public enum log_entry_type.dart
+    {
+        
+    }
+}
