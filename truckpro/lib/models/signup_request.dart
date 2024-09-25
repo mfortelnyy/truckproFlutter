1d89740dea28470af86d2a6430bbe94760136848
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-24 23:31:58 -0500

    Add confirm password attribute to the model

--- truckpro/lib/models/signup_request.dart
+++ truckpro/lib/models/signup_request.dart
@@ -2,13 +2,15 @@ class SignUpRequest {
   final String email;
   final String password;
   final String name;
+  final String confirmpassword;
 
-  SignUpRequest({required this.email, required this.password, required this.name});
+  SignUpRequest({required this.email, required this.password, required this.name, required this.confirmpassword});
 
   Map<String, dynamic> toJson() {
     return {
       'email': email,
       'password': password,
+      'confirmpassword': confirmpassword,
       'name': name,
     };
   }
