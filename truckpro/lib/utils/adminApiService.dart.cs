f09d8e99329f770ffd02a42b7ea2f877f51d5d6e
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-25 22:39:08 -0500

    Implement http requests for admin
    Make requests to API endpoints for admin

---
+++ truckpro/lib/utils/adminApiService.dart.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+import 'dart:convert'; //for JSON encoding/decoding
+import 'package:http/http.dart' as http; 
+
+namespace lib.utils
+{
+    public class adminApiService.dart
+    {
+        final String baseUrl = 'https://';
+
+        Future<List<dynamic>> getAllCompanies() async {
+            final response = await http.get(Uri.parse('$baseUrl/adm/getAllCompanies'));
+
+            if (response.statusCode == 200) {
+            return json.decode(response.body);
+            } else {
+            throw Exception('Failed to load companies');
+            }
+        }
+
+        Future<List<dynamic>> getAllDrivers() async {
+            final response = await http.get(Uri.parse('$baseUrl/adm/getAllDrivers'));
+
+            if (response.statusCode == 200) {
+            return json.decode(response.body);
+            } else {
+            throw Exception('Failed to load drivers');
+            }
+        }
+
+        Future<dynamic> getDriverById(int userId) async {
+            final response = await http.get(Uri.parse('$baseUrl/adm/getDriverById?userId=$userId'));
+
+            if (response.statusCode == 200) {
+            return json.decode(response.body);
+            } else {
+            throw Exception('Failed to load driver');
+            }
+        }
+
+        Future<List<dynamic>> getDriversByCompanyId(int companyId) async {
+            final response = await http.get(Uri.parse('$baseUrl/adm/getDriversByCompanyId?companyId=$companyId'));
+
+            if (response.statusCode == 200) {
+            return json.decode(response.body);
+            } else {
+            throw Exception('Failed to load drivers');
+            }
+        }
+
+        Future<List<dynamic>> getLogsByDriverId(int driverId) async {
+            final response = await http.get(Uri.parse('$baseUrl/adm/getLogsByDriverId?driverId=$driverId'));
+
+            if (response.statusCode == 200) {
+            return json.decode(response.body);
+            } else {
+            throw Exception('Failed to load logs');
+            }
+        }
+
+        Future<dynamic> createCompany(Map<String, dynamic> companyData) async {
+            final response = await http.post(
+                Uri.parse('$baseUrl/adm/createCompany'),
+                headers: {"Content-Type": "application/json"},
+                body: json.encode(companyData),
+            );
+
+            if (response.statusCode == 200) {
+            return json.decode(response.body);
+            } else {
+            throw Exception('Failed to create company');
+            }
+        }
+
+
+
+    }
+}
