26d2a2248d51e53d22bfb3895d9cb18a58e164d1
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-26 23:46:29 -0500

    Refactor SignInPage for Role-Based Navigation and Code Cleanup
    - Added role-based navigation in _handleSignIn() to redirect users based on their role (manager, driver, admin).
    -Replace non-const Text widgets with const for optimization in the error dialog and UI components.
    -Remove unused imports and commented-out code to improve readability.
    -Add JWT token decoding for role extraction.
    -Update the AppBar and other widget titles to const values for better performance.

--- truckpro/lib/views/DriverSigninPage.dart
+++ truckpro/lib/views/DriverSigninPage.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:truckpro/views/MyHomePage.dart';
+import 'package:truckpro/utils/adminApiService.dart';
+import 'package:truckpro/views/AdminHomePage.dart';
+//import 'package:truckpro/views/MyHomePage.dart';
+import 'package:truckpro/utils/login_service.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
-
+//import 'ManagerHomePage.dart'; 
+//import 'DriverHomePage.dart';  
 
 class SignInPage extends StatefulWidget {
   @override
@@ -11,21 +16,21 @@ class SignInPage extends StatefulWidget {
 class _SignInPageState extends State<SignInPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  
+  final LoginService _loginService = LoginService(); 
 
   void _showErrorDialog(String errorMessage) {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Error'),
+          title: const Text('Error'),
           content: Text(errorMessage),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('OK'),
+              child: const Text('OK'),
             ),
           ],
         );
@@ -33,18 +38,42 @@ class _SignInPageState extends State<SignInPage> {
     );
   }
 
+  //handle sign in and navigation based on role
   void _handleSignIn(BuildContext context) async {
     final email = _emailController.text;
     final password = _passwordController.text;
+    print('${email} + "    " + ${password}');
+    String? token = await _loginService.loginUser(email, password);
+    print("token: ${token}");
+    if (token != null && token.length>50) {
+      //decode JWT token to get the role
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+      //get the role from the token 
+      String role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']; 
+      print('role: ${role}');
 
-    final user = null;//await dbHelper.getUserByEmail(email);
-    
-    if (user != null && user.password == password) {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (context) => MyHomePage(user),
-        ),
-      );
+      //if (!mounted) return;
+
+
+      //navigate to the appropriate homepage based on the role
+      if (role == 'manager') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token,)),
+        );
+      } else if (role == 'driver') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token,)),
+        );
+      } else if (role == 'admin') {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token,)),
+        );
+      } else {
+        //default
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token,)),
+        );
+      }
     } else {
       _showErrorDialog('Invalid email or password');
     }
@@ -54,7 +83,7 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign In'),
+        title: const Text('Sign In'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -63,24 +92,21 @@ class _SignInPageState extends State<SignInPage> {
           children: <Widget>[
             TextField(
               controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: const InputDecoration(labelText: 'Email'),
             ),
             TextField(
               controller: _passwordController,
               obscureText: true,
-              decoration: InputDecoration(labelText: 'Password'),
+              decoration: const InputDecoration(labelText: 'Password'),
             ),
             ElevatedButton(
               onPressed: () => _handleSignIn(context),
-              child: Text('Log In'),
+              child: const Text('Log In'),
             ),
           ],
         ),
       ),
     );
   }
-  
-  String sha_256(String s){
-    return s;
-  }
 }
+
