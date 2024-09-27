47b7e16e78760d98ec3f9321ebe6bf84704a173a
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-26 22:48:01 -0500

    Update main for debugging

--- truckpro/lib/main.dart
+++ truckpro/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'views/DriverSigninPage.dart';
+import 'utils/adminApiService.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    final AdminApiService adminService = AdminApiService();
+
+    return MaterialApp(
+      home: SignInPage(),
     );
   }
 }
+
+
+
+
