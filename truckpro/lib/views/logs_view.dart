ba31f3c4e741f372d38cc5573d9f0435411fc53c
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-30 22:20:33 -0500

    Create screen for displaying Log Entries

---
+++ truckpro/lib/views/logs_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:truckpro/models/log_entry.dart';
+
+class LogsView extends StatelessWidget {
+  final List<LogEntry> logs;
+
+  LogsView({required this.logs});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Logs')),
+      body: ListView.builder(
+        itemCount: logs.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(logs[index].logEntryType.toString()),
+          );
+        },
+      ),
+    );
+  }
+}
