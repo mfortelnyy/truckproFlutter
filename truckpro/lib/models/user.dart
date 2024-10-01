91687821c13c5e49d3173f05d06ab7e348df173d
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-09-30 22:19:53 -0500

    Update user.dart
    Add the required attributes to match backend models

--- truckpro/lib/models/user.dart
+++ truckpro/lib/models/user.dart
@@ -1,31 +1,55 @@
 class User {
-  final String id;
+  final int id;
+  final String firstName;
+  final String lastName;
   final String email;
-  final String name;
+  final String password;
+  final String phone;
+  final int role;
+  final int companyId;
   final bool emailVerified;
+  final String? emailVerificationToken;
 
-  User({
-    required this.id,
+  User(
+  {
+    required this.id,    
+    required this.firstName,
+    required this.lastName,
     required this.email,
-    required this.name,
+    required this.password,
+    required this.phone,
+    required this.role,
+    required this.companyId,
     required this.emailVerified,
+    required this.emailVerificationToken,    
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
       email: json['email'],
-      name: json['name'],
+      password: json['password'],
+      phone: json['phone'],
+      role: json['role'],
+      companyId: json['companyId'],
       emailVerified: json['emailVerified'],
+      emailVerificationToken: json['emailVerificationToken']
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
       'email': email,
-      'name': name,
+      'phone': phone,
+      'role': role,
+      'companyId': companyId,
       'emailVerified': emailVerified,
+      'emailVerificationToken': emailVerificationToken
     };
   }
 }
