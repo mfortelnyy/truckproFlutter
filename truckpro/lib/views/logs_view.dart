e5671e011a4811cdbdc9685a5c2e962d15b57d9b
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-01 23:19:00 -0500

    Add future builder for displaying logs of the drivers

--- truckpro/lib/views/logs_view.dart
+++ truckpro/lib/views/logs_view.dart
@@ -2,20 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:truckpro/models/log_entry.dart';
 
 class LogsView extends StatelessWidget {
-  final List<dynamic> logs;
+  final Future<List<LogEntry>> logsFuture;  
 
-  LogsView({required this.logs});
+  const LogsView({super.key, required this.logsFuture});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Logs')),
-      body: ListView.builder(
-        itemCount: logs.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(logs[index].logEntryType.toString()),
-          );
+      body: FutureBuilder<List<dynamic>>(
+        future: logsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No logs found'));
+          } else {
+            final logs = snapshot.data!;
+            return ListView.builder(
+              itemCount: logs.length,
+              itemBuilder: (context, index) {
+                var log = logs[index];
+                return ListTile(
+                  title: Text('Log Entry Type: ${log['logEntryType']}'),
+                  subtitle: Text('Start Time: ${log['startTime']}'), // Assuming log entry has a startTime
+                );
+              },
+            );
+          }
         },
       ),
     );
