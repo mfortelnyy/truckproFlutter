bd86e363f8be418f56292cbd7252db9839161995
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-01 23:18:46 -0500

    Add future builder for displaying drivers

--- truckpro/lib/views/drivers_view.dart
+++ truckpro/lib/views/drivers_view.dart
@@ -1,34 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:truckpro/models/user.dart';
 import '../utils/admin_api_service.dart';
-import '../models/log_entry.dart';
 import 'logs_view.dart';
 
 class DriversView extends StatelessWidget {
-  final List<dynamic> drivers;
   final AdminApiService adminService;
-  final String token;
+  final Future<List<User>> driversFuture;  
 
-  const DriversView({super.key, required this.drivers, required this.adminService, required this.token});
+  const DriversView({super.key, required this.adminService, required this.driversFuture});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Drivers')),
-      body: ListView.builder(
-        itemCount: drivers.length,
-        itemBuilder: (context, index) {
-          var driver = drivers[index];
-          return ListTile(
-            title: Text(driver['name']),
-            onTap: () async {
-              // fetch logs for this driver
-              List<LogEntry> logs = await AdminApiService().getLogsByDriverId(driver['id']);
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LogsView(logs: logs)),
-              );
-            },
-          );
+      body: FutureBuilder<List<User>>(
+        future: driversFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No logs found'));
+          } else {
+            final drivers = snapshot.data!;
+            return ListView.builder(
+              itemCount: drivers.length,
+              itemBuilder: (context, index) {
+                var driver = drivers[index];
+                return ListTile(
+                    title: Text(driver.firstName),
+                    subtitle: Text(driver.email),
+                    onTap: () async {
+                      // fetch logs for this driver
+                      var logs =  adminService.getLogsByDriverId(driver.id);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LogsView(logsFuture: logs)),
+                      );
+                    },
+                );
+              }
+            );
+          }
         },
       ),
     );
