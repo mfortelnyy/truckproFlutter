536d044911b4f29ce641b0963f56c603d4649f1c
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-02 02:25:09 -0500

    Create create_company_screen.dart

---
+++ truckpro/lib/views/create_company_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import '../models/company.dart';
+import '../utils/admin_api_service.dart';
+
+class CreateCompanyScreen extends StatefulWidget {
+  final AdminApiService adminService;
+  final String token;
+
+  const CreateCompanyScreen({super.key, required this.adminService, required this.token});
+
+  @override
+  CreateCompanyScreenState createState() => CreateCompanyScreenState();
+}
+
+class CreateCompanyScreenState extends State<CreateCompanyScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _companyNameController = TextEditingController();
+  bool _isLoading = false;
+
+  @override
+  void dispose() {
+    _companyNameController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _createCompany() async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        _isLoading = true;
+      });
+      
+      try {
+        String companyName = _companyNameController.text.trim();
+        Company newCompany =  Company(name: companyName);
+        await widget.adminService.createCompany(newCompany, widget.token);
+
+        // success message 
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Company "$companyName" created successfully!')),
+        );
+
+        // navigate back after creation
+        Navigator.pop(context);
+      } catch (e) {
+        // Handle error
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error creating company: $e')),
+        );
+      } finally {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Create Company'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _companyNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Company Name',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a company name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              _isLoading
+                  ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: _createCompany,
+                      child: const Text('Create New Company'),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
