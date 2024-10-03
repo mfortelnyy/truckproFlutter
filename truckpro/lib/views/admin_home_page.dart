3889471c7e4acfb5d307e60b970ad8945fc6cb58
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-03 15:57:51 -0500

    Style Admin Home Page

--- truckpro/lib/views/admin_home_page.dart
+++ truckpro/lib/views/admin_home_page.dart
@@ -34,114 +34,154 @@ class AdminHomePageState extends State<AdminHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Admin Dashboard'),
+        title: const Text(
+          'Admin Dashboard',
+          style: TextStyle(
+            color: Colors.black, // Make AppBar text black to match the theme
+          ),
+        ),
+        backgroundColor: Colors.white, // White AppBar for a clean look
+        iconTheme: const IconThemeData(color: Colors.black), // Black icons in AppBar
+        elevation: 0, // Flat design
       ),
       drawer: _buildDrawer(context),
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder<List<Company>>(
-              future: _companies,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No companies found'));
-                } else {
-                  return Column(
-                    children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            var company = snapshot.data![index];
-                            return ListTile(
-                              title: Text(company.name), 
-                              subtitle: Text('ID: ${company.id}'),
-                              onTap: () {
-                                Future<List<User>> drivers = widget.adminService.getDriversByCompanyId(company.id, widget.token);
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => DriversView(driversFuture: drivers, adminService: widget.adminService,companyName: company.name,),
-                                  ),
-                                );
-                              },
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder<List<Company>>(
+                future: _companies,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No companies found'));
+                  } else {
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              var company = snapshot.data![index];
+                              return Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                elevation: 2,
+                                child: ListTile(
+                                  title: Text(company.name, style: const TextStyle(color: Colors.black)),
+                                  subtitle: Text('ID: ${company.id}', style: TextStyle(color: Colors.grey[600])),
+                                  onTap: () {
+                                    Future<List<User>> drivers = widget.adminService.getDriversByCompanyId(company.id, widget.token);
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => DriversView(
+                                          driversFuture: drivers, 
+                                          adminService: widget.adminService,
+                                          companyName: company.name,
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => CompaniesView(companiesFuture: _companies, token: widget.token),
+                              ),
                             );
                           },
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => CompaniesView(companiesFuture: _companies, token: widget.token),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 241, 158, 89), // Orange color for button
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                          );
-                        },
-                        child: const Text('Show All Companies'),
-                      ),
-                    ],
-                  );
-                }
-              },
+                          ),
+                          child: const Text('Show All Companies', style: TextStyle(color: Colors.white)),
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
             ),
-          ),
-          Expanded(
-            child: FutureBuilder<List<User>>(
-              future: _drivers,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return const Center(child: Text('No drivers found'));
-                } else {
-                  return Column(
-                    children: [
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: snapshot.data!.length,
-                          itemBuilder: (context, index) {
-                            var driver = snapshot.data![index];
-                            return ListTile(
-                              title: Text(driver.firstName),
-                              subtitle: Text('Driver ID: ${driver.id}'),
-                              onTap: () {
-                                var logs = widget.adminService.getLogsByDriverId(driver.id);
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => LogsView(logsFuture: logs),
-                                  ),
-                                );
-                              },
+            const SizedBox(height: 16),
+            Expanded(
+              child: FutureBuilder<List<User>>(
+                future: _drivers,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return const Center(child: Text('No drivers found'));
+                  } else {
+                    return Column(
+                      children: [
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              var driver = snapshot.data![index];
+                              return Card(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                elevation: 2,
+                                child: ListTile(
+                                  title: Text(driver.firstName, style: const TextStyle(color: Colors.black)),
+                                  subtitle: Text('Driver ID: ${driver.id}', style: TextStyle(color: Colors.grey[600])),
+                                  onTap: () {
+                                    var logs = widget.adminService.getLogsByDriverId(driver.id);
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => LogsView(logsFuture: logs),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DriversView(adminService: widget.adminService, driversFuture: _drivers, companyName: null,),
+                              ),
                             );
                           },
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => DriversView(adminService: widget.adminService, driversFuture: _drivers, companyName: null,),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 241, 158, 89), // Orange color for button
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                          );
-                        },
-                        child: const Text('Show All Drivers'),
-                      ),
-                    ],
-                  );
-                }
-              },
+                          ),
+                          child: const Text('Show All Drivers', style: TextStyle(color: Colors.white)),
+                        ),
+                      ],
+                    );
+                  }
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -153,13 +193,13 @@ class AdminHomePageState extends State<AdminHomePage> {
         children: <Widget>[
           const DrawerHeader(
             decoration: BoxDecoration(
-              color: Colors.blue,
+              color: Color.fromARGB(255, 241, 158, 89), // Orange background for drawer header
             ),
             child: Text('Admin Menu', style: TextStyle(color: Colors.white, fontSize: 24)),
           ),
           ListTile(
-            leading: Icon(Icons.business),
-            title: const Text('Create Company'),
+            leading: const Icon(Icons.business, color: Colors.black), // Black icons
+            title: const Text('Create Company', style: TextStyle(color: Colors.black)),
             onTap: () {
               Navigator.push(
                 context,
@@ -170,8 +210,8 @@ class AdminHomePageState extends State<AdminHomePage> {
             },
           ),
           ListTile(
-            leading: Icon(Icons.person_add),
-            title: const Text('Sign Up Manager'),
+            leading: const Icon(Icons.person_add, color: Colors.black), // Black icons
+            title: const Text('Sign Up Manager', style: TextStyle(color: Colors.black)),
             onTap: () {
               Navigator.push(
                 context,
