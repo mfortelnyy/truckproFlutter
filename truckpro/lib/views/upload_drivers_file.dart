4af91733aee564e038bb0244b8b12e8e58c0696f
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-03 23:07:56 -0500

    Create upload_drivers_file.dart

---
+++ truckpro/lib/views/upload_drivers_file.dart
@@ -0,0 +1,101 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class UploadDriversScreen extends StatefulWidget {
+  final String token;
+  final dynamic managerApiService;
+
+  const UploadDriversScreen({super.key, required this.managerApiService, required this.token});
+
+  @override
+  _UploadDriversScreenState createState() => _UploadDriversScreenState();
+}
+
+class _UploadDriversScreenState extends State<UploadDriversScreen> {
+  String? _fileName;
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  Future<void> _pickFile() async {
+    setState(() {
+      _errorMessage = null;
+    });
+
+    try {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['xls', 'xlsx'], // only  Excel files
+      );
+
+      if (result != null) {
+        PlatformFile file = result.files.first;
+
+        setState(() {
+          _fileName = file.name;
+        });
+
+        // Call your upload function here
+        _uploadFile(file.path!);
+      } else {
+        setState(() {
+          _errorMessage = "No file selected";
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = "Error while picking the file: $e";
+      });
+    }
+  }
+
+  
+  Future<void> _uploadFile(String filePath) async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      
+      await widget.managerApiService.uploadDriverEmails(filePath, widget.token);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('File uploaded successfully!')),
+      );
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+        _errorMessage = "File upload failed: $e";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upload Driver Emails'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_fileName != null) Text('Selected file: $_fileName'),
+            if (_errorMessage != null) Text(_errorMessage!, style: TextStyle(color: Colors.red)),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickFile,
+              child: Text('Pick Excel File'),
+            ),
+            const SizedBox(height: 20),
+            if (_isLoading) CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
