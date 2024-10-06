a2bb049bfa73682c3ae6b27bcf05994b6824f704
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-06 14:36:36 -0500

    Update log_entry.dart

--- truckpro/lib/models/log_entry.dart
+++ truckpro/lib/models/log_entry.dart
@@ -1,28 +1,37 @@
+import 'package:truckpro/models/log_entry_type.dart';
+import 'user.dart';
+
 class LogEntry {
   final int id;
   final int userId;
+  final User? user;
   final DateTime startTime;
-  final DateTime endTime;
-  final String logEntryType;
-  final List<String>? images;
+  final DateTime? endTime;
+  final LogEntryType logEntryType;
+  final List<String>? images; 
+  final bool? isApprovedByManager;
 
   LogEntry({
     required this.id,
     required this.userId,
     required this.startTime,
-    required this.endTime,
+    this.endTime,
     required this.logEntryType,
     this.images,
+    this.isApprovedByManager,
+    this.user,
   });
 
   factory LogEntry.fromJson(Map<String, dynamic> json) {
     return LogEntry(
       id: json['id'],
       userId: json['userId'],
+      user: json['user'] != null ? User.fromJson(json['user']) : null,
       startTime: DateTime.parse(json['startTime']),
-      endTime: DateTime.parse(json['endTime']),
-      logEntryType: json['logEntryType'],
-      images: json['images'] != null ? List<String>.from(json['images']) : null,
+      endTime: json['endTime'] != null ? DateTime.parse(json['endTime']) : null,
+      logEntryType: LogEntryType.values[json['logEntryType']],
+      images: json['imageUrls'] != null ? List<String>.from(json['imageUrls']) : null,
+      isApprovedByManager: json['isApprovedByManager'],
     );
   }
 
@@ -30,10 +39,12 @@ class LogEntry {
     return {
       'id': id,
       'userId': userId,
+      'user': user?.toJson(), 
       'startTime': startTime.toIso8601String(),
-      'endTime': endTime.toIso8601String(),
-      'logEntryType': logEntryType,
-      'images': images,
+      'endTime': endTime?.toIso8601String(),
+      'logEntryType': logEntryType.index, 
+      'imageUrls': images,
+      'isApprovedByManager': isApprovedByManager,
     };
   }
 }
