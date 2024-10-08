cf78cf6d5f9564efa6a21b5aae78a495275ba7c1
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-07 20:42:05 -0500

    Change datatype of logEntry
    now int to match the DTO received from API

--- truckpro/lib/models/log_entry.dart
+++ truckpro/lib/models/log_entry.dart
@@ -1,50 +1,52 @@
-import 'package:truckpro/models/log_entry_type.dart';
-import 'user.dart';
+import 'package:truckpro/models/user.dart';
 
 class LogEntry {
   final int id;
   final int userId;
-  final User? user;
+  final User? user; 
   final DateTime startTime;
-  final DateTime? endTime;
-  final LogEntryType logEntryType;
-  final List<String>? images; 
-  final bool? isApprovedByManager;
+  final DateTime? endTime; 
+  final int logEntryType;
+  final List<String>? imageUrls; 
+  final bool isApprovedByManager;
 
   LogEntry({
     required this.id,
     required this.userId,
+    this.user,
     required this.startTime,
     this.endTime,
     required this.logEntryType,
-    this.images,
-    this.isApprovedByManager,
-    this.user,
+    this.imageUrls,
+    required this.isApprovedByManager,
   });
 
+  // Factory method to create a LogEntry from JSON
   factory LogEntry.fromJson(Map<String, dynamic> json) {
     return LogEntry(
       id: json['id'],
       userId: json['userId'],
-      user: json['user'] != null ? User.fromJson(json['user']) : null,
+      user: json['user'] !=null ? User.fromJson(json['user']) : null,
       startTime: DateTime.parse(json['startTime']),
       endTime: json['endTime'] != null ? DateTime.parse(json['endTime']) : null,
-      logEntryType: LogEntryType.values[json['logEntryType']],
-      images: json['imageUrls'] != null ? List<String>.from(json['imageUrls']) : null,
+      logEntryType: json['logEntryType'],
+      imageUrls: json['imageUrls'] != null ? List<String>.from(json['imageUrls']) : null,
       isApprovedByManager: json['isApprovedByManager'],
     );
   }
 
+  // Method to convert a LogEntry to JSON
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'userId': userId,
-      'user': user?.toJson(), 
+      'user': user,
       'startTime': startTime.toIso8601String(),
       'endTime': endTime?.toIso8601String(),
-      'logEntryType': logEntryType.index, 
-      'imageUrls': images,
+      'logEntryType': logEntryType,
+      'imageUrls': imageUrls,
       'isApprovedByManager': isApprovedByManager,
     };
   }
 }
+
