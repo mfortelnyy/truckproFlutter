d7148478e48858c8a5f28058fa88c136074a9481
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-07 22:40:26 -0500

    Update pending_users_view.dart

--- truckpro/lib/views/pending_users_view.dart
+++ truckpro/lib/views/pending_users_view.dart
@@ -1,47 +1,72 @@
 import 'package:flutter/material.dart';
-import '../models/pending_user.dart';
+import 'package:truckpro/models/pending_user.dart';
+import '../utils/admin_api_service.dart';
+import 'logs_view.dart';
 
 class PendingUsersView extends StatelessWidget {
-  final List<PendingUser> pendingUsers;
+  
+  final Future<List<PendingUser>> pendingUsersFuture;  
+  final String token;
 
-  const PendingUsersView({super.key, required this.pendingUsers});
+  const PendingUsersView({
+    super.key,
+    required this.pendingUsersFuture,
+    required this.token,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pending Users'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: pendingUsers.isNotEmpty
-            ? ListView.builder(
-                itemCount: pendingUsers.length,
-                itemBuilder: (context, index) {
-                  final pendingUser = pendingUsers[index];
-                  return Card(
-                    margin: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: ListTile(
-                      title: Text(pendingUser.email),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Created: ${pendingUser.createdDate.toLocal()}'),
-                          Text('Company ID: ${pendingUser.companyId}'),
-                          Text('Invitation Sent: ${pendingUser.invitationSent ? "Yes" : "No"}'),
-                        ],
-                      ),
-                      trailing: pendingUser.invitationSent
-                          ? const Icon(Icons.check_circle, color: Colors.green)
-                          : const Icon(Icons.error, color: Colors.red),
+      appBar: AppBar(title: const Text('Drivers'), backgroundColor: Color.fromARGB(255, 241, 158, 89),),
+      body: FutureBuilder<List<PendingUser>>(
+        future: pendingUsersFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No drivers found for company!'));
+          } else {
+            final pendingUsers = snapshot.data!;
+            return ListView.builder(
+              itemCount: pendingUsers.length,
+              itemBuilder: (context, index) {
+                var pUser = pendingUsers[index];
+                return Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  elevation: 4,
+                  child: ListTile(
+                    title: Text('User with id: ${pUser.id}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Email: ${pUser.email}'),
+                        Text('Date Created: ${pUser.createdDate}'),
+                        Text('Company Id: ${pUser.companyId}'),
+                        Text('Invitation Sent: ${pUser.invitationSent ? "Yes" : "No"}'),
+                      ],
                     ),
-                  );
-                },
-              )
-            : const Center(
-                child: Text('No pending users found.'),
-              ),
+                  ),
+                );
+              },
+            );
+          }
+        },
       ),
     );
+  }
+}
+
+roleToString(int role) {
+  switch (role) {
+    case 0:
+      return "Admin";
+    case 1:
+      return "Manager";
+    case 2:
+      return "Driver";
+    default:
+      return "default";
   }
 }
