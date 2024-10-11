406efc9f6d2231f63a78ceae876a9d750d03fbc3
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-10 22:59:16 -0500

    Update change_password_request.dart

--- truckpro/lib/models/change_password_request.dart
+++ truckpro/lib/models/change_password_request.dart
@@ -2,24 +2,19 @@ class ChangePasswordRequest {
   final int userId;
   final String oldPassword;
   final String newPassword;
-  final String confirmPassword;
   
 
   ChangePasswordRequest({
     required this.userId,
     required this.oldPassword,
-    required this.newPassword,
-    required this.confirmPassword,
-    
+    required this.newPassword,    
   });
 
   Map<String, dynamic> toJson() {
     return {
       'userId': userId,
       'oldPassword': oldPassword,
-      'newPassword': newPassword,
-      'confirmPassword': confirmPassword,
-      
+      'newPassword': newPassword
     };
   }
 }
