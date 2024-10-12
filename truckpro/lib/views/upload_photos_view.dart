486319b9cf89e1b19a4010e341d078cb7e509169
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-12 02:07:19 -0500

    Update upload_photos_view.dart

--- truckpro/lib/views/upload_photos_view.dart
+++ truckpro/lib/views/upload_photos_view.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/material.dart';
-import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:truckpro/utils/admin_api_service.dart';
+import 'dart:io';
+import '../utils/driver_api_service.dart';
 
 class UploadPhotosScreen extends StatefulWidget {
-  final Function(List<File>) callback; 
-
-  UploadPhotosScreen({required this.callback});
+  final String token;
+  
+  UploadPhotosScreen({required this.token});
 
   @override
-  _UploadPhotosScreenState createState() => _UploadPhotosScreenState();
+  _UploadPhotosScreenState createState() => _UploadPhotosScreenState(token: token);
 }
 
 class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
   final ImagePicker _picker = ImagePicker();
-  List<File> _selectedImages = [];
+  List<File> _images = [];
+  final String token;
+  _UploadPhotosScreenState({required this.token});
+
 
   Future<void> _pickImages() async {
-    final pickedFiles = await _picker.pickMultiImage();
-
-    if (pickedFiles != null) {
+    final List<XFile>? selectedImages = await _picker.pickMultiImage();
+    if (selectedImages != null) {
       setState(() {
-        _selectedImages =
-            pickedFiles.take(10).map((e) => File(e.path)).toList();
+        _images = selectedImages.map((image) => File(image.path)).toList();
       });
     }
   }
-
-  void _submit() {
-    if (_selectedImages.length > 0) {
-      widget.callback(_selectedImages); 
-      Navigator.pop(context); 
+  Future<void> _createLog() async {
+    if (_images.length == 10) {
+      DriverApiService driverService = DriverApiService(token: token);
+      await driverService.createDrivingLog(_images);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Photos uploaded successfully!'),
+      ));
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please select at least one image.')),
-      );
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Please select 10 photos!'),
+      ));
     }
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -45,30 +52,25 @@ class _UploadPhotosScreenState extends State<UploadPhotosScreen> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 5,
-                mainAxisSpacing: 5,
-              ),
-              itemCount: _selectedImages.length,
-              itemBuilder: (context, index) {
-                return Image.file(
-                  _selectedImages[index],
-                  fit: BoxFit.cover,
-                );
-              },
-            ),
-          ),
           ElevatedButton(
             onPressed: _pickImages,
-            child: Text('Select Images'),
+            child: Text('Select Photos'),
           ),
-          SizedBox(height: 10),
+          _images.isNotEmpty
+              ? GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  itemCount: _images.length,
+                  itemBuilder: (context, index) {
+                    return Image.file(_images[index]);
+                  },
+                )
+              : Text('No images selected'),
           ElevatedButton(
-            onPressed: _submit,
-            child: Text('Upload Selected Photos'),
+            onPressed: _createLog, 
+            child: Text('Send'),
           ),
         ],
       ),
