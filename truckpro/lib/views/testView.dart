61e988302e8f4d940c2d753aa57fff8f431c5cc9
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-14 23:46:01 -0500

    Create testView.dart

---
+++ truckpro/lib/views/testView.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const Scaffold(
+        body: Center(
+          child: TestView(),
+        ),
+      ),
+    );
+  }
+}
+
+class TestView extends StatefulWidget {
+  const TestView({Key? key}) : super(key: key);
+
+  @override
+  _TestViewState createState() => _TestViewState();
+}
+
+class _TestViewState extends State<TestView> {
+
+  /// declare a cound variable with initial value
+  int count = 0;
+
+  /// declare a timer
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// Initialize a periodic timer with 1 second duration
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        /// callback will be executed every 1 second, increament a count value 
+        /// on each callback
+        setState(() {
+          count++;
+        });
+      },
+    );
+  }
+
+  /// Since periodic timer doesn't cancels untill expicitely called
+  /// It is important to cancel them on dispose, so that it doesn't stays active
+  /// when widget is not binded to tree
+  @override
+  void dispose() {
+    super.dispose();
+    timer?.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Counter reached $count");
+  }
+}
