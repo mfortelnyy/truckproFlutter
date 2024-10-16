02e4ef80d39c89361f8ead9800366f31bd81aa38
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-15 21:31:30 -0500

    Add new function for driver to get a total off duty hours

--- truckpro/lib/utils/driver_api_service.dart
+++ truckpro/lib/utils/driver_api_service.dart
@@ -260,7 +260,26 @@ Future<String> createDrivingLog(List<String> filePaths) async {
 
   }
    
-  
+   Future<String> getTotalOffDutyHoursLastWeek() async { 
+    final response = await http.get(
+      Uri.parse('$_baseUrl/getTotalOffDutyHoursLastWeek'),
+      headers: {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      
+      return jsonDecode(response.body);
+      
+    } else {
+      throw Exception('Failed to load total Off Duty hours for driver: ${response.body}');
+    }
+
+
+  }
+
 }
 
 basename(String filePath) {
