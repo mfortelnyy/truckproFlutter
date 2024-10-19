9286cc32cbe4d5c3642b666b8098f2948c201126
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-19 01:44:16 -0500

    Use session manager to decide if already signed in

--- truckpro/lib/main.dart
+++ truckpro/lib/main.dart
@@ -1,23 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:truckpro/views/admin_home_page.dart';
+import 'utils/admin_api_service.dart';
+import 'views/driver_home_page.dart';
+import 'views/manager_home_page.dart';
 import 'views/user_signin_page.dart';
 
-
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return const MaterialApp(
-      home: SignInPage(),
+    return MaterialApp(
+      title: 'TruckPro',
+      home: SplashScreen(), //checks session
     );
   }
 }
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
 
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkSession();
+  }
 
+  Future<void> _checkSession() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? token = prefs.getString('authToken');
 
+    if (token != null && JwtDecoder.isExpired(token) == false) {
+      // Token is valid and not expired
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
+      String role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+      // Navigate based on role
+      switch (role) {
+        case "Admin":
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token)),
+          );
+          break;
+        case "Manager":
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => ManagerHomeScreen(token: token)),
+          );
+          break;
+        case "Driver":
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => DriverHomeView(token: token)),
+          );
+          break;
+        default:
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => SignInPage()),
+          );
+      }
+    } else {
+      // token is invalid or expired
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => SignInPage()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
