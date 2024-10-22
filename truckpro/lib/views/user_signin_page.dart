3d8270ea1372302d19adb246bc34ba75cf1092a8
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-21 22:20:03 -0500

    Add attributes for navigation

--- truckpro/lib/views/user_signin_page.dart
+++ truckpro/lib/views/user_signin_page.dart
@@ -11,13 +11,16 @@ import '../utils/session_manager.dart';
 import 'admin_home_page.dart';
 
 class SignInPage extends StatefulWidget {
-  const SignInPage({super.key});
+  final Function(bool) toggleTheme;
+  const SignInPage({super.key, required this.toggleTheme});
 
   @override
   SignInPageState createState() => SignInPageState();
 }
 
 class SignInPageState extends State<SignInPage> {
+  bool isDarkMode = false; 
+
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final LoginService _loginService = LoginService();
@@ -62,15 +65,15 @@ class SignInPageState extends State<SignInPage> {
       switch (role) {
         case "Admin":
           Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token)),
+          MaterialPageRoute(builder: (context) => AdminHomePage(adminService: AdminApiService(), token: token, sessionManager: _sessionManager, toggleTheme: widget.toggleTheme,)),
           );
         case "Manager":
           Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => ManagerHomeScreen(token: token)),
+          MaterialPageRoute(builder: (context) => ManagerHomeScreen(token: token, sessionManager: _sessionManager, toggleTheme: widget.toggleTheme,)),
           );
         case "Driver":
           Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => DriverHomeView(token: token, sessionManager: _sessionManager,)),
+          MaterialPageRoute(builder: (context) => DriverHomeView(token: token, sessionManager: _sessionManager, toggleTheme: widget.toggleTheme,)),
           );
           break;
         default:
@@ -82,144 +85,156 @@ class SignInPageState extends State<SignInPage> {
   }
    @override
    Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text('User Log In'),
-      backgroundColor: const Color.fromARGB(255, 241, 158, 89),
-      elevation: 10,
-    ),
-    body: Stack(
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/registration_bg.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Center(
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  TextField(
-                    controller: _emailController,
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      labelStyle: const TextStyle(color: Colors.black), 
-                      filled: true,
-                      fillColor: const Color.fromARGB(227, 238, 178, 127).withOpacity(0.75),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(
-                          color: Color.fromARGB(255, 109, 219, 236), 
-                          width: 2,
-                        ),
-                      ),
-                      floatingLabelStyle: const TextStyle(
-                        fontSize: 16,
-                        color: Color.fromARGB(255, 249, 249, 249), // label color when focused (floating above)
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 35),
-                  TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: const TextStyle(color: Colors.black),
-                      filled: true,
-                      fillColor: const Color.fromARGB(227, 238, 178, 127).withOpacity(0.75),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(color: Color.fromARGB(255, 109, 219, 236), width: 2.0),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: const BorderSide(color: Colors.white, width: 2.0),
-                      ),
-                      floatingLabelStyle: const TextStyle(
-                        fontSize: 16,
-                        color: Color.fromARGB(255, 249, 249, 249), 
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Align(
-                  alignment: Alignment.centerRight,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white.withOpacity(0.5), // Set border color with low opacity
-                        width: 2, // Border width
-                      ),
-                      borderRadius: BorderRadius.circular(20), // Circular border
-                    ),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => ForgotPasswordView()),
-                        );
-                      },
-                      child: const Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                          color: Color.fromARGB(198, 246, 241, 241),
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                 const SizedBox(height: 25),
-                  ElevatedButton(
-                    onPressed: () => _handleSignIn(context), 
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 66, 164, 70),
-                      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      textStyle: const TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                    child: const Text('Log In', style: TextStyle(color:Colors.white ),),
-                  ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                     onPressed: () => Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => const DriverSignupPage()),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 241, 158, 89),
-                      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      textStyle: const TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                    child: const Text('Sign Up', style: TextStyle(color:Colors.white ),),
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Log In'),
+        backgroundColor: const Color.fromARGB(255, 241, 158, 89),
+        elevation: 10,
+        actions: [
+          IconButton(
+            icon: Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode),
+            onPressed: () {
 
-                 ],
+              setState(() {
+                isDarkMode = !isDarkMode;
+              });
+              widget.toggleTheme(isDarkMode); 
+            },
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/registration_bg.png'),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-        ),
-      ],
-    ),
-  );
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Center(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: const TextStyle(color: Colors.black), 
+                        filled: true,
+                        fillColor: const Color.fromARGB(227, 238, 178, 127).withOpacity(0.75),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 109, 219, 236), 
+                            width: 2,
+                          ),
+                        ),
+                        floatingLabelStyle: const TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 249, 249, 249), // label color when focused (floating above)
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 35),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: const TextStyle(color: Colors.black),
+                        filled: true,
+                        fillColor: const Color.fromARGB(227, 238, 178, 127).withOpacity(0.75),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(color: Color.fromARGB(255, 109, 219, 236), width: 2.0),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.white, width: 2.0),
+                        ),
+                        floatingLabelStyle: const TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 249, 249, 249), 
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.5), // Set border color with low opacity
+                          width: 2, // Border width
+                        ),
+                        borderRadius: BorderRadius.circular(20), // Circular border
+                      ),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => ForgotPasswordView()),
+                          );
+                        },
+                        child: const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Color.fromARGB(198, 246, 241, 241),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                    ElevatedButton(
+                      onPressed: () => _handleSignIn(context), 
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 66, 164, 70),
+                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        textStyle: const TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      child: const Text('Log In', style: TextStyle(color:Colors.white ),),
+                    ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                      onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const DriverSignupPage()),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 241, 158, 89),
+                        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        textStyle: const TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      child: const Text('Sign Up', style: TextStyle(color:Colors.white ),),
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
 }
 
  }
