52c0dd2bfc5bbfb20dd79b57d8ce2799beb0ec0a
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-10-27 14:01:09 -0500

    Update main.dart

--- truckpro/lib/main.dart
+++ truckpro/lib/main.dart
@@ -51,7 +51,7 @@ class _MyAppState extends State<MyApp> {
 
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext   context) {
     return MaterialApp(
       title: 'TruckPro',
       theme: ThemeData(
