fafb75f56b5541f463b841b1653948687c27e015
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-13 23:13:10 -0600

    Serialize log types into a JSON to send to the endpoint the filter selection

--- truckpro/lib/utils/report_api_service.dart
+++ truckpro/lib/utils/report_api_service.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
@@ -6,13 +7,16 @@ import 'package:truckpro/models/log_entry_type.dart';
 class ReportApiService 
 {
   final String baseUrl = 'https://truckcheck.org:443';
-  
-  Future<Uint8List?> generatePDF(DateTime startDate, DateTime endDate, String token, int driverId,  List<LogEntryType> selectedLogTypes) async {
+
+  Future<Uint8List?> generatePDF(DateTime startDate, DateTime endDate, String token, int driverId, List<LogEntryType> selectedLogTypes) async {
     // format the dates to match the expected format in the backend
     String startDateString = DateFormat('yyyy-MM-dd').format(startDate);
     String endDateString = DateFormat('yyyy-MM-dd').format(endDate);
+    // serialize the selectedLogTypes list to a JSON string
+    String selectedLogTypesString = jsonEncode(selectedLogTypes.map((e) => e.toString().split(".").last).toList());
 
-    final url = Uri.parse('$baseUrl/getdrivingRecordsPDF?startDate=$startDateString&endDate=$endDateString&driverId=$driverId');
+
+    final url = Uri.parse('$baseUrl/getdrivingRecordsPDF?startDate=$startDateString&endDate=$endDateString&driverId=$driverId&selectedLogTypesJsonString=$selectedLogTypesString');
 
     final headers = {
       'Authorization': 'Bearer $token',  
