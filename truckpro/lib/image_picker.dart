320165276e545cf45c6a47670451c7eceeecc89b
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-14 18:44:19 -0600

    Add methods channel to open native ios image picker

---
+++ truckpro/lib/image_picker.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/services.dart';
+
+class NativeImagePicker {
+  static const MethodChannel _channel = MethodChannel('com.yourapp/image_picker');
+
+  //picks an image from either the camera or the gallery
+  static Future<String?> pickImage() async {
+    final String? imagePath = await _channel.invokeMethod('pickImage');
+    print(imagePath);
+    return imagePath;
+  }
+}
