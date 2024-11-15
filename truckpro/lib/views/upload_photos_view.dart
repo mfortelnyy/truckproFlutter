5ff10073646e4a539c6e021a71477d1b4d5763c1
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-14 21:53:46 -0600

    Update upload_photos_view.dart

--- truckpro/lib/views/upload_photos_view.dart
+++ truckpro/lib/views/upload_photos_view.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-
 import 'package:truckpro/utils/driver_api_service.dart';
 
 import '../image_picker.dart';
