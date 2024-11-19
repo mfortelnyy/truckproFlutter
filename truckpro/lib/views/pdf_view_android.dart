77e0ea256d9b688342567cc88f4f8bf56e8e0d2c
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-18 23:50:52 -0600

    Implement PDF Viewer for Android with a new package

--- truckpro/lib/views/pdf_view_android.dart
+++ truckpro/lib/views/pdf_view_android.dart
@@ -1,8 +1,8 @@
 import 'dart:typed_data';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:easy_pdf_viewer/easy_pdf_viewer.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdfx/pdfx.dart';
 
 class PDFViewerWidgetAndroid extends StatefulWidget {
   final Uint8List pdfBytes;
@@ -14,7 +14,7 @@ class PDFViewerWidgetAndroid extends StatefulWidget {
 }
 
 class _PDFViewerWidgetAndroidState extends State<PDFViewerWidgetAndroid> {
-  late Future<PDFDocument> _pdfDocumentFuture;
+  late Future<PdfDocument> _pdfDocumentFuture;
 
   @override
   void initState() {
@@ -22,36 +22,44 @@ class _PDFViewerWidgetAndroidState extends State<PDFViewerWidgetAndroid> {
     _pdfDocumentFuture = _loadPdfDocument();
   }
 
-  // Function to save the PDF bytes to a file and return the document
-  Future<PDFDocument> _loadPdfDocument() async {
-    // Get the temporary directory to save the file
+  Future<PdfDocument> _loadPdfDocument() async {
+    //temporary directory to save the file
     final tempDir = await getTemporaryDirectory();
     final tempFile = File('${tempDir.path}/temp_pdf.pdf');
 
-    // Save the PDF bytes to the file
+    //store PDF bytes to the file
     await tempFile.writeAsBytes(widget.pdfBytes);
 
-    // Load the PDF document from the saved file
-    return PDFDocument.fromFile(tempFile);
+    //load PDF document from the saved file
+    return PdfDocument.openFile(tempFile.path);
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<PDFDocument>(
-      future: _pdfDocumentFuture,
+    return FutureBuilder<PdfDocument>(
+      future: _pdfDocumentFuture,  
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
           return const Center(child: Text('Error loading PDF'));
         } else if (snapshot.hasData) {
-          final doc = snapshot.data;
+          final doc = snapshot.data;  
+          if (doc == null) {
+            return const Center(child: Text('Failed to load PDF'));
+          }
+
           return Scaffold(
             appBar: AppBar(
               title: const Text('PDF Viewer'),
             ),
-            body: PDFViewer(
-              document: doc!,
+            body: PdfView(
+              controller: PdfController(
+                document: Future.value(doc),  
+              ),
+              onDocumentLoaded: (document) {
+                //print('Document loaded successfully!');
+              },
             ),
           );
         } else {
