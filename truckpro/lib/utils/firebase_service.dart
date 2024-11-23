c637230175994250c171c2ae49bc470ea6bcb295
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-23 03:48:30 -0600

    Add local configurations for Firebase notifications

--- truckpro/lib/utils/firebase_service.dart
+++ truckpro/lib/utils/firebase_service.dart
@@ -1,8 +1,44 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:truckpro/utils/session_manager.dart';
 
 class FirebaseService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin _localNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  final SessionManager _sessionManager = SessionManager();
+  
+  FirebaseService() {
+    // local notifications
+    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const  iOS = DarwinInitializationSettings();
+    const initSettings = InitializationSettings(android: android, iOS: iOS);
 
+    _localNotificationsPlugin.initialize(initSettings);
+
+    //subscribe to a company topic based on companyId
+    void subscribeToCompanyTopic() async {
+      try {
+        String? jwtToken = await _sessionManager.getToken();
+
+        Map<String, dynamic> decodedToken = JwtDecoder.decode(jwtToken!);
+
+        String companyId = decodedToken['companyId']; 
+
+        await _firebaseMessaging.subscribeToTopic(companyId);
+        print("Subscribed to topic: $companyId");
+      } catch (e) {
+        print("Error subscribing to topic: $e");
+      }
+    }
+    
+    //foreground message handler
+    configureForegroundMessageHandler();
+    
+    // background message handler
+    FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  }
+  
   //retrieve current FCM token for the device
   Future<String?> getDeviceToken() async {
     try {
@@ -14,6 +50,8 @@ class FirebaseService {
       return null;
     }
   }
+
+  
 
   // listen for FCM token refresh
   void onTokenRefresh(Function(String) callback) {
