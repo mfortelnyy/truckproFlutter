cff55b07167ef3c18b5e0433d7155675c56fd6dd
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-27 01:12:03 -0600

    Add File selector for ios

--- truckpro/macos/Flutter/GeneratedPluginRegistrant.swift
+++ truckpro/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,22 +5,20 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
+import file_selector_macos
 import firebase_core
 import firebase_messaging
 import flutter_local_notifications
 import path_provider_foundation
-import pdfx
 import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
