20dbce189a44314d50bf64b2f383092e3abc2897
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-27 01:13:09 -0600

    Add Registration of device for push Notifications

--- truckpro/macos/Runner/AppDelegate.swift
+++ truckpro/macos/Runner/AppDelegate.swift
@@ -1,9 +1,51 @@
-import Cocoa
-import FlutterMacOS
+import UIKit
+import Flutter
+import FirebaseCore
+import FirebaseMessaging
 
-@NSApplicationMain
-class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+    
+    UNUserNotificationCenter.current().delegate = self
+    Messaging.messaging().delegate = self
+    requestNotificationAuthorization(application: application)
+    
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  func requestNotificationAuthorization(application: UIApplication) {
+    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+      if granted {
+        DispatchQueue.main.async {
+          application.registerForRemoteNotifications()
+        }
+      }
+    }
+  }
+
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    if let token = fcmToken {
+      print("Firebase registration token: \(token)")
+    }
+  }
+
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    Messaging.messaging().apnsToken = deviceToken
+  }
+
+  override func application(
+    _ application: UIApplication,
+    didFailToRegisterForRemoteNotificationsWithError error: Error
+  ) {
+    print("Failed to register for remote notifications: \(error)")
   }
 }
