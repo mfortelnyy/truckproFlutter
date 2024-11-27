79f8a08a35882fa680d4b2960eb493708c503bd8
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-11-27 01:17:09 -0600

    Change bundle id for android

--- truckpro/android/app/src/main/kotlin/com/example/truckpro/MainActivity.kt
+++ truckpro/android/app/src/main/kotlin/com/example/truckpro/MainActivity.kt
@@ -1,5 +1,17 @@
-package com.example.truckpro
+package com.truckpro.appdev
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.embedding.engine.FlutterEngine
+import android.Manifest
+import android.widget.Toast
 
-class MainActivity: FlutterActivity()
+
+class MainActivity : FlutterActivity() 
