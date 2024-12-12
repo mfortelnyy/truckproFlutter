8a7f1ae3276116763aba83e3482da3549c469df2
mfortelnyy <mfortelnyy@hawk.iit.edu>
2024-12-12 15:57:54 -0600

    Update model to accommodate for new data transfer object

--- truckpro/lib/models/log_entry.dart
+++ truckpro/lib/models/log_entry.dart
@@ -1,15 +1,20 @@
 import 'package:truckpro/models/user.dart';
-
 import 'log_entry_type.dart';
 
 class LogEntry {
   final int id;
   final int userId;
-  final User? user; 
+  final User? user;
   final DateTime startTime;
-  final DateTime? endTime; 
+  final DateTime? endTime;
   final LogEntryType logEntryType;
-  final List<String>? imageUrls; 
+  final List<String>? imageUrls;
+  // Parents can be On Duty and Off Duty only 
+  //so child Log entries like Break and Driving will have id for the Parent Log it's associated
+  final int? parentLogEntryId; 
+  //On Duty can contain Driving and Break
+  //Off duty can contain Break which will be displayed as 'Sleep'
+  final List<LogEntry>? childLogEntries; // New field
   final bool isApprovedByManager;
 
   LogEntry({
@@ -20,6 +25,8 @@ class LogEntry {
     this.endTime,
     required this.logEntryType,
     this.imageUrls,
+    this.parentLogEntryId, 
+    this.childLogEntries, 
     required this.isApprovedByManager,
   });
 
@@ -28,11 +35,17 @@ class LogEntry {
     return LogEntry(
       id: json['id'],
       userId: json['userId'],
-      user: json['user'] !=null ? User.fromJson(json['user']) : null,
+      user: json['user'] != null ? User.fromJson(json['user']) : null,
       startTime: DateTime.parse(json['startTime']),
       endTime: json['endTime'] != null ? DateTime.parse(json['endTime']) : null,
-      logEntryType: LogEntryType.values[json['logEntryType']], 
+      logEntryType: LogEntryType.values[json['logEntryType']],
       imageUrls: json['imageUrls'] != null ? List<String>.from(json['imageUrls']) : null,
+      parentLogEntryId: json['parentLogEntryId'],
+      childLogEntries: json['childLogEntries'] != null
+          ? (json['childLogEntries'] as List)
+              .map((entry) => LogEntry.fromJson(entry))
+              .toList()
+          : null, 
       isApprovedByManager: json['isApprovedByManager'],
     );
   }
@@ -42,13 +55,14 @@ class LogEntry {
     return {
       'id': id,
       'userId': userId,
-      'user': user,
+      'user': user?.toJson(),
       'startTime': startTime.toIso8601String(),
       'endTime': endTime?.toIso8601String(),
       'logEntryType': logEntryType.index,
       'imageUrls': imageUrls,
+      'parentLogEntryId': parentLogEntryId, 
+      'childLogEntries': childLogEntries?.map((entry) => entry.toJson()).toList(), 
       'isApprovedByManager': isApprovedByManager,
     };
   }
 }
-
